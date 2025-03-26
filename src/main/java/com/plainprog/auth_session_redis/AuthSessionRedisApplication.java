9f986dd7c8d917acbfab6c84e96ce604adc67eb4
Petro Darchyn <petrodarchyn@gmail.com>
2025-03-26 19:40:10 +0100

    first commit

---
+++ src/main/java/com/plainprog/auth_session_redis/AuthSessionRedisApplication.java
@@ -0,0 +1,13 @@
+package com.plainprog.auth_session_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSessionRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSessionRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plainprog/auth_session_redis/AuthSessionRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plainprog.auth_session_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSessionRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
